0ec1c934a920ee7f8e54d3116e6ccd7d56ce7e1e
Liam Nelson <lmjnelson@yahoo.com>
2025-02-05 16:55:35 +0000

    Update default mail name and address

--- www/config/mail.php
+++ www/config/mail.php
@@ -108,8 +108,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'hello@starfish.envx'),
+        'name' => env('MAIL_FROM_NAME', 'hello@starfish.envx'),
     ],
 
     /*
