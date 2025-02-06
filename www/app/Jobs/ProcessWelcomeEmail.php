01fce0f4fc59d4dd992abcec1fb1a42162f4325b
Liam Nelson <lmjnelson@yahoo.com>
2025-02-06 15:46:58 +0000

    add functionality to process onboarding emails via job queues

--- www/app/Jobs/ProcessWelcomeEmail.php
+++ www/app/Jobs/ProcessWelcomeEmail.php
@@ -2,20 +2,16 @@
 
 namespace App\Jobs;
 
-use App\Mail\VerifyEmail;
 use App\Mail\WelcomeMail;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Auth\User;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
-use Throwable;
 
-class ProcessUserOnboarding implements ShouldQueue
+class ProcessWelcomeEmail implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
@@ -35,15 +31,6 @@ class ProcessUserOnboarding implements ShouldQueue
      */
     public function handle(): void
     {
-            try {
-                ProcessWelcomeEmail::dispatch($this->user);
-                ProcessEmailVerification::dispatch($this->user)->delay(now()->addSeconds(120));
-            }  catch (Throwable $e) {
-                Log::error("ProcessUserOnboarding job failed: " . $e->getMessage(), [
-                    'user_id' => $this->user->id,
-                    'exception' => $e
-                ]);
-                $this->fail($e);
-            }
+        Mail::to($this->user->email)->send(new WelcomeMail($this->user));
     }
 }
--- www/app/Providers/EventServiceProvider.php
+++ www/app/Providers/EventServiceProvider.php
@@ -5,7 +5,6 @@ namespace App\Providers;
 use App\Events\UserRegistered;
 use App\Listeners\UserHasRegistered;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
